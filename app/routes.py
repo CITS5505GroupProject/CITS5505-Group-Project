5525a485da60e40dc55d7cff35f914c5080c57d1
Siyuan Zhou <23861658@student.uwa.edu.au>
2024-04-30 14:12:12 +0800

    adding sample server-side rendering

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World! This is index"
+    user = {'username':'Tony', 'DoB':'10/12/2001'}
+    return render_template('index.html', title = 'Home Page', user = user)
