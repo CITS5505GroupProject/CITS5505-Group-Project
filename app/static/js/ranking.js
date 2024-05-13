1a4f616796c3a1a4e42948018595f1d74237b008
qianqianli626 <qianqianli626@gmail.com>
2024-05-14 03:31:01 +0800

    changed the style of ranking page

--- app/static/js/ranking.js
+++ app/static/js/ranking.js
@@ -1,43 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const accordion = document.querySelector('.accordion');
-    const panel = document.querySelector('.panel');
-
-    if (accordion) {
-        accordion.addEventListener('click', function () {
-            this.classList.toggle('active');
-            if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            }
-        });
-    }
-
-    const dropdown = document.querySelector('.dropdown');
-    const dropdownContent = document.querySelector('.dropdown-content');
-
-    // 保证在悬停时保持下拉框显示
-    dropdown.addEventListener('mouseenter', function () {
-        dropdownContent.style.display = 'block';
-    });
-
-    dropdownContent.addEventListener('mouseenter', function () {
-        dropdownContent.style.display = 'block';
-    });
-
-    dropdown.addEventListener('mouseleave', function () {
-        setTimeout(() => {
-            if (!dropdownContent.matches(':hover')) {
-                dropdownContent.style.display = 'none';
-            }
-        }, 100);
-    });
-
-    dropdownContent.addEventListener('mouseleave', function () {
-        setTimeout(() => {
-            if (!dropdown.matches(':hover')) {
-                dropdownContent.style.display = 'none';
-            }
-        }, 100);
+    const getMorePointsButton = document.querySelector('.circle-button');
+    
+    getMorePointsButton.addEventListener('click', function () {
+        window.location.href = '/otherforms';
     });
 });
