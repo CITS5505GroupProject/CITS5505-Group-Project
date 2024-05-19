36e4aaa078dc029176c8643b4f6f717158f308a4
chinmaiii <cravindran01@gmail.com>
2024-05-19 09:16:21 +0800

    Revert "Merge branch 'main' of https://github.com/CITS5505GroupProject/CITS5505-Group-Project into chinmai"
    This reverts commit 475f002f0987124e90cbb129fb9847e80f68c104, reversing
    changes made to 5b32953f2b1f150cb7b71056cde7149caf6eb568.

--- app/__init__.py
+++ app/__init__.py
@@ -1,66 +1,39 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
+import os
 from flask_mail import Mail
 
-db = SQLAlchemy()
-migrate = Migrate()
-login_manager = LoginManager()
-csrf = CSRFProtect()
-mail = Mail()
 
-class TestConfig:
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    WTF_CSRF_ENABLED = False
-    SECRET_KEY = "TESTING_KEY"
+app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.dirname(basedir)
+print(parent_dir)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(parent_dir, 'app.db')
+app.config['SECRET_KEY'] = 'you_will_not_guess'
+app.config['UPLOAD_FOLDER'] = os.path.join(parent_dir, 'app/static/user_profile')
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB limit
 
-def create_app(config_name=None):
-    app = Flask(__name__)
+#email setup
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'surveystream10@gmail.com'
+app.config['MAIL_PASSWORD'] = 'gaxh zpcp gvmz mvog' #app password
+app.config['MAIL_DEFAULT_SENDER'] = 'surveystream10@gmail.com'
 
-    if config_name is None:
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        parent_dir = os.path.dirname(basedir)
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(parent_dir, 'app.db')
-        app.config['SECRET_KEY'] = 'DEPLOY_KEY'
-    else:
-        app.config.from_object(TestConfig)
+mail = Mail(app)
+csrf=CSRFProtect(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
+import models, routes
 
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    parent_dir = os.path.dirname(basedir)
-    app.config['UPLOAD_FOLDER'] = os.path.join(parent_dir, 'app/static/user_profile')
-    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB limit
-
-    # Email setup
-    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'surveystream10@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'gaxh zpcp gvmz mvog'  # App password
-    app.config['MAIL_DEFAULT_SENDER'] = 'surveystream10@gmail.com'
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-    login_manager.init_app(app)
-    csrf.init_app(app)
-    mail.init_app(app)
-
-    login_manager.login_view = 'main.login'
-
-    with app.app_context():
-        from . import models
-        models.db.create_all()
-
-        from app.main import main as main_blueprint
-        app.register_blueprint(main_blueprint)
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return models.User.query.get(int(user_id))
-
-    return app
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.query.get(int(user_id))
